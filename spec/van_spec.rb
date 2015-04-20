2c8f8f396b74571106cb8d6aca899468e9031a91
Sara OC <saramoo@hotmail.com>
2015-04-20 23:02:36 +0100

    small refactor on van test double

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -4,6 +4,7 @@ describe Van do
   let(:van) { Van.new(capacity: 6) }
   let(:working_bike) { double :working_bike, broken?: false }
   let(:broken_bike) { double :broken_bike, broken?: true }
+  let(:docking_station) { double :docking_station, bikes:([working_bike, broken_bike]) }
 
   it 'has a capacity when created' do
     expect(van.capacity).to be(6)
@@ -18,7 +19,6 @@ describe Van do
 
   it 'removes broken bikes from a station when collecting' do
     # This docking station is not a real one. It cannot change. It will not pay attention to any method calls.
-    docking_station = double :docking_station, bikes:([working_bike, broken_bike])
     # Since it cannot change, you have to tell it to receive the method BEFORE you call the method.
     expect(docking_station).to receive(:release).with broken_bike
     van.collect_broken_bikes_from docking_station
