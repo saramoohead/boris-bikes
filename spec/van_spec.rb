2046d5bee4a905d554637a9b42424b496dadf243
Sara OC <saramoo@hotmail.com>
2015-04-20 23:03:22 +0100

    finishing small refactor

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -11,15 +11,12 @@ describe Van do
   end
 
   it 'collects broken bikes from a station' do
-    docking_station = double :docking_station, bikes:([working_bike, broken_bike])
     allow(docking_station).to receive(:release)
     van.collect_broken_bikes_from docking_station
     expect(van.bikes).to eq([broken_bike])
   end
 
   it 'removes broken bikes from a station when collecting' do
-    # This docking station is not a real one. It cannot change. It will not pay attention to any method calls.
-    # Since it cannot change, you have to tell it to receive the method BEFORE you call the method.
     expect(docking_station).to receive(:release).with broken_bike
     van.collect_broken_bikes_from docking_station
   end
