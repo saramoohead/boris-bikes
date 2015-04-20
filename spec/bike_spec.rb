5027e4ed2e97e4e473e6ab8b1bf16d10a7dfcfec
Sara OC <saramoo@hotmail.com>
2015-04-20 10:12:02 +0100

    bike can be fixed

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,14 +1,21 @@
 require 'bike'
 
 describe Bike do
+
+  let(:bike) { Bike.new }
+
   it 'when created should not be broken' do
-    bike = Bike.new
     expect(bike).not_to be_broken
   end
 
   it 'is able to break' do
-    bike = Bike.new
     bike.break!
     expect(bike).to be_broken
+  end
+
+  it 'is able to be fixed' do
+    bike.break!
+    bike.fix!
+    expect(bike).not_to be_broken
   end
 end
